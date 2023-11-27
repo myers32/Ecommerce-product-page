fc8f3f01df199ba4ba58712d017ce6fcff497f7b
Dawid Osiński <dawidosinski59@gmail.com>
2023-11-27 14:38:37 +0100

    nextImage function added

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,26 @@
-const next = document.querySelector(".next-js");
 const previous = document.querySelector(".previous-js");
 const images = document.querySelector(".slider__images").children;
 const totalImages = images.length;
 let currentIndex = 0;
 
-next.addEventListener("click", () => {
-	images[currentIndex].classList.remove("main");
-	
-	if (currentIndex === totalImages - 1) {
-		currentIndex = 0;
-	} else {
-		currentIndex++;
-	}
 
-	images[currentIndex].classList.add("main");
-});
+const nextImage = () => {
+	const next = document.querySelector(".next-js");
+
+	next.addEventListener("click", () => {
+		images[currentIndex].classList.remove("main");
+
+		if (currentIndex === totalImages - 1) {
+			currentIndex = 0;
+		} else {
+			currentIndex++;
+		}
+
+		images[currentIndex].classList.add("main");
+	});
+};
+
+const init = () => {
+	nextImage();
+};
+init();
