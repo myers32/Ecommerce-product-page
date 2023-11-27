5ff9416c5d7308343532836e0f5fdeca2c7d8d24
Dawid Osiński <dawidosinski59@gmail.com>
2023-11-27 17:28:08 +0100

    counter fun added + slider fun separeted

--- app/js/script.js
+++ app/js/script.js
@@ -1,45 +1,82 @@
 {
-	const images = document.querySelector(".slider__images").children;
-	const totalImages = images.length;
-	let currentIndex = 0;
+	const slider = () => {
+
+		const images = document.querySelector(".slider__images").children;
+		const totalImages = images.length;
+		let currentIndex = 0;
 
 
-	const nextImage = () => {
-		const next = document.querySelector(".next-js");
+		const nextImage = () => {
+			const next = document.querySelector(".next-js");
 
-		next.addEventListener("click", () => {
-			images[currentIndex].classList.remove("main");
+			next.addEventListener("click", () => {
+				images[currentIndex].classList.remove("main");
 
-			if (currentIndex === totalImages - 1) {
-				currentIndex = 0;
-			} else {
-				currentIndex++;
-			}
+				if (currentIndex === totalImages - 1) {
+					currentIndex = 0;
+				} else {
+					currentIndex++;
+				}
 
-			images[currentIndex].classList.add("main");
-		});
+				images[currentIndex].classList.add("main");
+			});
+		};
+
+		const prevImage = () => {
+			const previous = document.querySelector(".previous-js");
+
+			previous.addEventListener("click", () => {
+				images[currentIndex].classList.remove("main");
+
+				if (currentIndex === 0) {
+					currentIndex = totalImages - 1;
+				} else {
+					currentIndex--;
+				}
+
+				images[currentIndex].classList.add("main");
+			});
+		};
+
+		const init = () => {
+			nextImage();
+			prevImage();
+		};
+
+		init();
 	};
 
-	const prevImage = () => {
-		const previous = document.querySelector(".previous-js");
 
-		previous.addEventListener("click", () => {
-			images[currentIndex].classList.remove("main");
 
-			if (currentIndex === 0) {
-				currentIndex = totalImages - 1;
-			} else {
-				currentIndex--;
-			}
+	const counter = () => {
+		let amount = 0;
+		let result = document.querySelector(".result-js");
 
-			images[currentIndex].classList.add("main");
-		});
+		const increaseAmount = () => {
+			const increase = document.querySelector(".increase-js");
+
+			increase.addEventListener("click", () => {
+				amount++;
+				result.innerHTML = amount;
+			});
+		};
+
+		const decreaseAmount = () => {
+			const decrease = document.querySelector(".decrease-js");
+
+			decrease.addEventListener("click", () => {
+
+				if (amount > 0) {
+					amount--;
+					result.innerHTML = amount;
+				}
+			});
+		};
+
+		decreaseAmount();
+		increaseAmount();
 	};
 
-	const init = () => {
-		nextImage();
-		prevImage();
-	};
-	
-	init();
+	slider();
+	counter();
 }
