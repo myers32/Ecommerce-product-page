c548c8104b3568db929d618bd2496f0c08db48f7
Dawid Osiński <dawidosinski59@gmail.com>
2023-11-27 14:20:05 +0100

    event listener added to next btn

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,17 @@
-console.log('HELLO');
+const next = document.querySelector(".next-js");
+const previous = document.querySelector(".previous-js");
+const images = document.querySelector(".slider__images").children;
+const totalImages = images.length;
+let currentIndex = 0;
 
-const test = () => {
-	console.log('this is a test');
-};
+next.addEventListener("click", () => {
+	images[currentIndex].classList.remove("main");
+	
+	if (currentIndex === totalImages - 1) {
+		currentIndex = 0;
+	} else {
+		currentIndex++;
+	}
+
+	images[currentIndex].classList.add("main");
+});
